aea5d4031e8154ca99f425a84de7fdec7511ac56
aziznal <abodenaal@gmail.com>
2020-08-31 01:12:48 +0300

    Update project for new BankSpider

--- CustomExceptions.py
+++ CustomExceptions.py
@@ -9,3 +9,8 @@ class CurrencyNotFoundException(Exception):
 class TableNotFoundException(NoSuchElementException):
     def __init__(self, msg=None, screen=None, stacktrace=None):
         super().__init__(msg=msg, screen=screen, stacktrace=stacktrace)
+
+
+class StalledSpyderException(RuntimeError):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
